3fb5ecbb99637bfe0b29b303e50d17ab1d85d11d
Vlad D. <vladdidyk1985@gmail.com>
2026-01-05 17:39:08 +0200

    chore: jwt config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 
 import {ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
+    AuthModule,
+    UserModule
   ],
 })
 export class AppModule {}
