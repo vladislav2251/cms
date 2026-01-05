9291b48e1fe411a0ee2c5c3bb64dbe788e666b2d
Vlad D. <vladdidyk1985@gmail.com>
2026-01-05 17:34:31 +0200

    chore: main.ts app.module config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import cookieParser from 'cookie-parser'
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(cookieParser())
+  app.enableCors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true,
+    exposedHeaders: 'set-cookie'
+  })
+
+  await app.listen(process.env.SERVER_URL ?? 5000);
 }
 bootstrap();
